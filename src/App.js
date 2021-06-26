c1dc833b8af5cda5da1682c6de5c3598b42e09f2
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-26 10:47:40 -0400

    Add multiple components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navebar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new blog";
@@ -10,16 +11,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navebar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times </p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>boo</p>
-        <p>{"hello"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google site</a>
+        <Home />
       </div>
     </div>
   );
