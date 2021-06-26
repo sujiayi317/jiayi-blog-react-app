3f78a52e8a2ffc9cb9bce00f398ef568f25ec2fc
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-26 11:15:25 -0400

    Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hi", e);
+  };
+  const handleClickAgain = (name, e) => {
+    console.log("hi " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("name", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
