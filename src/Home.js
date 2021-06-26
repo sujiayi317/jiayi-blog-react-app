f1e51bfc1674e4810e570707b86baa7490ed32f4
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-26 15:06:18 -0400

    Function as props

--- src/Home.js
+++ src/Home.js
@@ -13,12 +13,18 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id != id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === "jenny")}
         title="Jenny's blogs"
+        handleDelete={handleDelete}
       />
     </div>
   );
