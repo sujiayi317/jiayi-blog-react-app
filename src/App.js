abb14f4e672cc987fe9edf44fc7c5dff769d991b
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-26 10:18:56 -0400

    Insert dynamic value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  // const person = { name: "Jenny", age: 30 };
+  const boo = true;
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times </p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>boo</p>
+        <p>{"hello"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google site</a>
+      </div>
     </div>
   );
 }
