ee0bc72fe11fa42d720e6e6ee279a4de77f6c0ae
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-26 22:04:06 -0400

    Conditional loading message and handle fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,34 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // console.log(err);
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       {blogs && (
         <BlogList
